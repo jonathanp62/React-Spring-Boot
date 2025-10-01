2296ba8e27bb2f10f1906482c83be9efc12beb31
Jonathan Parker <jonathanp62@gmail.com>
2025-10-01 15:46:30 -0400

    Updated because of the move of the Tables.css file

--- src/components/person/PersonCreator.jsx
+++ src/components/person/PersonCreator.jsx
@@ -28,7 +28,7 @@
  * SOFTWARE.
  */
 
-import "../Tables.css";
+import "../styles/Tables.css";
 
 import React, { useState } from "react";
 
--- src/components/quote/QuoteComponent.jsx
+++ src/components/quote/QuoteComponent.jsx
@@ -28,7 +28,7 @@
  * SOFTWARE.
  */
 
-import "../Tables.css";
+import "../styles/Tables.css";
 
 import React, { useState, useEffect } from 'react';
 
